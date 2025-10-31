ddb47e20f29a954a0449c37eedc428b3c2522e66
Sean Hageman <Sean@SEANIMAC.local>
2025-10-31 17:01:31 -0400

    Setup Custom Domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shageman.github.io/',
-  base: '/alvana/',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
