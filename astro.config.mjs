0f7ade6f2ace57f792ee231fa0030d756d7b3f46
Sean Hageman <Sean@SEANIMAC.local>
2025-10-31 17:14:53 -0400

    added under construction

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://shageman.github.io/',
+  site: 'https://alvana.io/',
   base: '/',
   vite: {
     plugins: [tailwindcss()]
