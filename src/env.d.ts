60f0504fd95d53ff75864676b0050694680f6fb2
Sean Hageman <Sean@SEANIMAC.local>
2026-07-14 17:57:33 -0400

    Bot abuse protection

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly PUBLIC_SUPABASE_URL: string;
   readonly PUBLIC_SUPABASE_ANON_KEY: string;
+  readonly PUBLIC_TURNSTILE_SITE_KEY: string;
 }
 
 interface ImportMeta {
