2713fc78c0597e2e999afcefa6f9b20ec0c0664b
Sean Hageman <Sean@SEANIMAC.local>
2026-07-15 12:25:42 -0400

    Pofile and menu upgrades

---
+++ src/lib/avatars.ts
@@ -0,0 +1,11 @@
+import avatar1 from '../assets/avatars/avatar-1.svg';
+import avatar2 from '../assets/avatars/avatar-2.svg';
+import avatar3 from '../assets/avatars/avatar-3.svg';
+import avatar4 from '../assets/avatars/avatar-4.svg';
+import avatar5 from '../assets/avatars/avatar-5.svg';
+import avatar6 from '../assets/avatars/avatar-6.svg';
+import avatar7 from '../assets/avatars/avatar-7.svg';
+import avatar8 from '../assets/avatars/avatar-8.svg';
+import avatar9 from '../assets/avatars/avatar-9.svg';
+
+export const avatars = [avatar1, avatar2, avatar3, avatar4, avatar5, avatar6, avatar7, avatar8, avatar9];
